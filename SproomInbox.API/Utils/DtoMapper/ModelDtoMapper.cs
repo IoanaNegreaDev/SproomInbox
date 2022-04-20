765bbb466726d70eeb9db034bff5691c41332922
Ioana Negrea <johannabeardesign@gmail.com>
2022-04-20 12:17:16 +0300

    API - Removed Enum extension

--- SproomInbox.API/Utils/DtoMapper/ModelDtoMapper.cs
+++ SproomInbox.API/Utils/DtoMapper/ModelDtoMapper.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using SproomInbox.API.Domain.Models;
-using SproomInbox.API.Utils.Extensions;
 using SproomInbox.API.Utils.Paging;
 using SproomInbox.WebApp.Shared.Resources;
 
@@ -11,16 +10,16 @@ namespace SproomInbox.API.Utils.DtoMapper
         public ModelDtoMapper()
         {
             CreateMap<User, UserDto>();
-   
+
             CreateMap<Document, DocumentDto>()
                   .ForMember(dest => dest.State,
-                             opt => opt.MapFrom(src => src.StateId.ToDescriptionString()))
+                             opt => opt.MapFrom(src => Enum.GetName(typeof(State), src.StateId)))
                   .ForMember(dest => dest.Type,
-                             opt => opt.MapFrom(src => src.TypeId.ToDescriptionString()));
+                             opt => opt.MapFrom(src => src.TypeId.ToString()));
 
             CreateMap<DocumentState, DocumentStateDto>()
                   .ForMember(dest => dest.State,
-                             opt => opt.MapFrom(src => src.StateId.ToDescriptionString()));
+                             opt => opt.MapFrom(src => Enum.GetName(typeof(State), src.StateId)));
 
             CreateMap(typeof(PagedList<Document>), typeof(PagedList<DocumentDto>)).ConvertUsing(typeof(PagedListMapper<Document, DocumentDto>));
         }  
